c5a945471ae70e1cf78f7bf78048e3590f08b038
Deivid Andreis <obreezygamer@gmail.com>
2022-06-06 02:17:03 -0300

    ultima modificação

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ function calcular(){
     document.getElementById('resultado').innerHTML = "<p>Seu imc é: <p/>"+imc.toFixed(1)+mensagem;
     
 
-    
+    //vou fazer um merge para a main
     
 }
